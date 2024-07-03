3a3f57b498a2133677fbf4ce1deb822a6e0c0583
resident7709 <resident7709@gmail.com>
2024-07-03 20:26:25 +0300

    check the project as a whole

--- script.js
+++ script.js
@@ -1,74 +1,74 @@
-const modal = document.getElementById("modal");
-const modalShow = document.getElementById("show-modal");
-const modalClose = document.getElementById("close-modal");
-const bookmarkForm = document.getElementById("bookmark-form");
-const websiteNameEl = document.getElementById("website-name");
-const websiteUrlEl = document.getElementById("website-url");
-const bookmarksContainer = document.getElementById("bookmarks-container");
+const modal = document.getElementById('modal');
+const modalShow = document.getElementById('show-modal');
+const modalClose = document.getElementById('close-modal');
+const bookmarkForm = document.getElementById('bookmark-form');
+const websiteNameEl = document.getElementById('website-name');
+const websiteUrlEl = document.getElementById('website-url');
+const bookmarksContainer = document.getElementById('bookmarks-container');
 
 let bookmarks = {};
 
-// *Show Modal, Focus on Input
+// * Show Modal, Focus on Input
 function showModal() {
-  modal.classList.add("show-modal");
+  modal.classList.add('show-modal');
   websiteNameEl.focus();
 }
 
-// *Modal event listeners
-modalShow.addEventListener("click", showModal);
-modalClose.addEventListener("click", () =>
-  modal.classList.remove("show-modal")
+// * Modal event listeners
+modalShow.addEventListener('click', showModal);
+modalClose.addEventListener('click', () =>
+  modal.classList.remove('show-modal')
 );
-window.addEventListener("click", (e) =>
-  e.target === modal ? modal.classList.remove("show-modal") : false
+window.addEventListener('click', e =>
+  e.target === modal ? modal.classList.remove('show-modal') : false
 );
 
-// *Validate Form
+// * Validate Form
 function validate(nameValue, urlValue) {
   const expression =
     /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/g;
   const regex = new RegExp(expression);
   if (!nameValue || !urlValue) {
-    alert("Please submit values for both fields");
+    alert('Please submit values for both fields');
     return false;
   }
   if (!urlValue.match(regex)) {
-    alert("Please provide a valid address");
+    alert('Please provide a valid address');
     return false;
   }
-  // *Valid
+  // Valid
   return true;
 }
 
-// *Build Bookmarks DOM
+// * Build Bookmarks DOM
 function buildBookmarks() {
   // Remove all bookmarks elements
-  bookmarksContainer.textContent = "";
+  bookmarksContainer.textContent = '';
   // Build Items
-  bookmarks.forEach((bookmark) => {
+  bookmarks.forEach(bookmark => {
     const { name, url } = bookmark;
     // Item
-    const item = document.createElement("div");
-    item.classList.add("item");
+    const item = document.createElement('div');
+    item.classList.add('item');
     // Close Icon
-    const closeIcon = document.createElement("i");
-    closeIcon.classList.add("fas", "fa-times");
-    closeIcon.setAttribute("title", "Delete Bookmark");
-    closeIcon.setAttribute("onclick", `deleteBookmark('${url}')`);
+    const closeIcon = document.createElement('i');
+    closeIcon.classList.add('fas', 'fa-times');
+    closeIcon.setAttribute('title', 'Delete Bookmark');
+    closeIcon.setAttribute('onclick', `deleteBookmark('${url}')`);
     // Favicon / Link Container
-    const linkInfo = document.createElement("div");
-    linkInfo.classList.add("name");
+    const linkInfo = document.createElement('div');
+    linkInfo.classList.add('name');
     // Favicon
-    const favicon = document.createElement("img");
+    const favicon = document.createElement('img');
     favicon.setAttribute(
-      "src",
+      'src',
       `https://www.google.com/s2/favicons?domain=${url}`
     );
-    favicon.setAttribute("alt", "Favicon");
+    favicon.setAttribute('alt', 'Favicon');
     // Link
-    const link = document.createElement("a");
-    link.setAttribute("href", `${url}`);
-    link.setAttribute("target", "_blank");
+    const link = document.createElement('a');
+    link.setAttribute('href', `${url}`);
+    link.setAttribute('target', '_blank');
     link.textContent = name;
     // Append to bookmarks container
     linkInfo.append(favicon, link);
@@ -77,25 +77,25 @@ function buildBookmarks() {
   });
 }
 
-// *Fetch Bookmarks
+// * Fetch Bookmarks
 function fetchBookmarks() {
   // Get bookmarks from localStorage if available
-  if (localStorage.getItem("bookmarks")) {
-    bookmarks = JSON.parse(localStorage.getItem("bookmarks"));
+  if (localStorage.getItem('bookmarks')) {
+    bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
   } else {
     // Create bookmarks array in localStorage
     bookmarks = [
       {
-        name: "Рог Носорога",
-        url: "https://dharmasite.ru/library1/466/",
+        name: 'Рог Носорога',
+        url: 'https://dharmasite.ru/library1/466/',
       },
     ];
-    localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
   }
   buildBookmarks();
 }
 
-// *Delete Bookmark
+// * Delete Bookmark
 function deleteBookmark(url) {
   // Loop through the bookmarks array
   bookmarks.forEach((bookmark, i) => {
@@ -104,16 +104,16 @@ function deleteBookmark(url) {
     }
   });
   // Update bookmarks array in localStorage, re-populate DOM
-  localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
+  localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
   fetchBookmarks();
 }
 
-// *Handle data from form
+// * Handle data from form
 function storeBookmark(e) {
   e.preventDefault();
   const nameValue = websiteNameEl.value;
   let urlValue = websiteUrlEl.value;
-  if (!urlValue.includes("https://") && !urlValue.includes("http://")) {
+  if (!urlValue.includes('https://') && !urlValue.includes('http://')) {
     urlValue = `https://${urlValue}`;
   }
   if (!validate(nameValue, urlValue)) {
@@ -124,14 +124,14 @@ function storeBookmark(e) {
     url: urlValue,
   };
   bookmarks.push(bookmark);
-  localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
+  localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
   fetchBookmarks();
   bookmarkForm.reset();
   websiteNameEl.focus();
 }
 
-// *General event listener
-bookmarkForm.addEventListener("submit", storeBookmark);
+// * General event listener
+bookmarkForm.addEventListener('submit', storeBookmark);
 
-// *Onload, Fetch Bookmarks
+// * Onload, Fetch Bookmarks
 fetchBookmarks();
